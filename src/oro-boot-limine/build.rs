f67b03fd56fc756f4be008672d30afc6db4f0558
Josh Junon <junon@oro.sh>
2023-10-11 16:36:37 +0200

    fix cross-compilation of limine bootloader

--- src/oro-boot-limine/build.rs
+++ src/oro-boot-limine/build.rs
@@ -1,7 +1,12 @@
 fn main() {
-	#[cfg(target_arch = "x86_64")]
-	{
-		println!("cargo:rustc-link-arg-bin=oro-boot-limine=-T");
-		println!("cargo:rustc-link-arg-bin=oro-boot-limine=src/oro-boot-limine/link/x86_64.ld",);
+	let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+	match target_arch.as_str() {
+		"x86_64" => {
+			println!("cargo:rustc-link-arg-bin=oro-boot-limine=-T");
+			println!("cargo:rustc-link-arg-bin=oro-boot-limine=src/oro-boot-limine/link/x86_64.ld",);
+		}
+		target_arch => {
+			panic!("unknown or unsupported architecture: {target_arch}");
+		}
 	}
 }
