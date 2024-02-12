6e1e01c61c2428c44cee57eb6e39aa519e14bded
Josh Junon <junon@oro.sh>
2024-02-12 19:09:42 +0100

    limine: add module request handling

--- oro-bootloader-limine/src/lib.rs
+++ oro-bootloader-limine/src/lib.rs
@@ -8,7 +8,15 @@
 #![no_std]
 #![deny(missing_docs)]
 
-use oro_common::{dbg, dbg_err, dbg_warn, Arch};
+use core::ffi::CStr;
+use limine::{modules::InternalModule, request::ModuleRequest};
+use oro_common::{dbg, dbg_err, Arch};
+
+const KERNEL_PATH: &CStr = limine::cstr!("/oro-kernel");
+
+#[used]
+static REQ_MODULES: ModuleRequest =
+	ModuleRequest::new().with_internal_modules(&[&InternalModule::new().with_path(KERNEL_PATH)]);
 
 /// Runs the Limine bootloader.
 ///
@@ -21,9 +29,27 @@ pub unsafe fn init<A: Arch>() -> ! {
 	A::init_shared();
 	A::init_local();
 
-	dbg!(A, "limine", "general");
-	dbg_warn!(A, "limine", "warning");
-	dbg_err!(A, "limine", "error");
+	let module_response = match REQ_MODULES.get_response() {
+		Some(modules) => modules,
+		None => {
+			dbg_err!(A, "limine", "module request failed");
+			A::halt()
+		}
+	};
+
+	let kernel_module = module_response
+		.modules()
+		.iter()
+		.find(|module| module.path() == KERNEL_PATH.to_bytes());
+	let _kernel_module = match kernel_module {
+		Some(module) => module,
+		None => {
+			dbg_err!(A, "limine", "failed to find kernel module: {KERNEL_PATH:?}");
+			A::halt()
+		}
+	};
+
+	dbg!(A, "limine", "kernel module found");
 
 	A::halt() // TODO(qix-): Temporary.
 }
@@ -33,6 +59,7 @@ pub unsafe fn init<A: Arch>() -> ! {
 /// # Safety
 /// Do **NOT** call this function directly.
 /// It is only called by the architecture-specific binaries.
-pub unsafe fn panic<A: Arch>(_info: &::core::panic::PanicInfo) -> ! {
+pub unsafe fn panic<A: Arch>(info: &::core::panic::PanicInfo) -> ! {
+	dbg_err!(A, "limine", "panic: {:?}", info);
 	A::halt()
 }
