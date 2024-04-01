7831cb8ebd8b0e6cfdb02fb8546d209af31aef2e
Josh Junon <junon@oro.sh>
2024-04-01 16:27:05 +0200

    limine: document private items

--- oro-bootloader-limine/src/lib.rs
+++ oro-bootloader-limine/src/lib.rs
@@ -6,7 +6,11 @@
 //! and individual, architecture-specific binaries.
 //! See the `bin/` directory for architecture-specific entry points.
 #![no_std]
-#![deny(missing_docs, clippy::integer_division)]
+#![deny(
+	missing_docs,
+	clippy::integer_division,
+	clippy::missing_docs_in_private_items
+)]
 #![feature(type_alias_impl_trait)]
 
 use core::ffi::CStr;
@@ -26,27 +30,69 @@ use oro_common::{
 	Arch, PrebootConfig, PrebootPrimaryConfig,
 };
 
+/// The path to where the Oro kernel is expected.
+/// The bootloader does **not** expect it to be listed
+/// as a module (but it can be).
 const KERNEL_PATH: &CStr = limine::cstr!("/oro-kernel");
 
+/// Provides Limine with a base revision of the protocol
+/// that this "kernel" (in Limine terms) expects.
 #[used]
 static BASE_REVISION: BaseRevision = BaseRevision::new();
+
+/// Requests a list of modules provided to the kernel via
+/// Limine configuration.
 #[used]
 static REQ_MODULES: ModuleRequest = ModuleRequest::with_revision(1)
 	.with_internal_modules(&[&InternalModule::new().with_path(KERNEL_PATH)]);
+
+/// Requests that Limine performs a Higher Half Direct Map (HHDM)
+/// of all physical memory. Provides an offset for the HHDM.
+///
+/// Note that the boot stage does not rely on an identity map as we
+/// will overwrite certain lower-half memory mappings when implementing
+/// the stubs (as prescribed by the Oro architectures Limine supports).
 #[used]
 static REQ_HHDM: HhdmRequest = HhdmRequest::with_revision(0);
+
+/// Requests a physical memory map from Limine.
 #[used]
 static REQ_MMAP: MemoryMapRequest = MemoryMapRequest::with_revision(0);
+
+/// Requests the BIOS timestamp from Limine.
 #[used]
 static REQ_TIME: BootTimeRequest = BootTimeRequest::with_revision(0);
+
+/// Requests that Limine initializes secondary cores and provides
+/// us a way to instruct them to jump to the boot stage entry point.
+///
+/// Note that the mere presence of this request causes Limine to
+/// bootstrap those cores.
+///
+/// Marked as mutable since we have to write to the `GotoAddress` field.
 #[used]
 static mut REQ_SMP: SmpRequest = SmpRequest::with_revision(0);
+
+/// In debug builds, stack size is very quickly exhausted. At time
+/// of writing, Limine allocates 64KiB of stack space per core, but
+/// this is not enough for debug builds.
+///
+/// Further, since there are no stack fences or automatic stack growing
+/// implemented in this stage, we must ensure there's enough stack space
+/// available for the debug build to avoid a stack overflow and subsequent
+/// corruption of kernel memory.
+///
+/// Thus, we expand the stack size here, fairly substantially.
 #[cfg(debug_assertions)]
 #[used]
 static REQ_STKSZ: StackSizeRequest = StackSizeRequest::with_revision(0).with_size(256 * 1024);
 
+/// A TAIT definition that extracts the type of the memory region iterator
+/// without needing to spell it out in full.
 type LimineMemoryRegionIterator = impl Iterator<Item = LimineMemoryRegion> + Clone + 'static;
 
+/// Macro to get a response from a request, panicking if it fails.
+/// All request fetches must go through this macro.
 macro_rules! get_response {
 	($req:ident, $label:literal) => {{
 		let Some(r) = $req.get_response() else {
@@ -150,6 +196,11 @@ pub unsafe fn init<A: Arch, C: CpuId>() -> ! {
 	})
 }
 
+/// Creates a memory map iterator from the Limine memory map response,
+/// which maps the Limine memory map types to Oro memory map types.
+///
+/// This is split out solely for the purpose of populating the [`LimineMemoryRegionIterator`]
+/// with the implicit type of the iterator without needing to spell it out.
 fn make_memory_map_iterator() -> LimineMemoryRegionIterator {
 	let mmap_response = get_response!(REQ_MMAP, "memory mapping");
 
@@ -202,6 +253,8 @@ pub unsafe fn panic<A: Arch>(info: &::core::panic::PanicInfo) -> ! {
 	A::halt()
 }
 
+/// Provides Limine-specific types to the boot sequence for use
+/// in initializing and booting the Oro kernel.
 struct LiminePrimaryConfig;
 
 impl PrebootPrimaryConfig for LiminePrimaryConfig {
@@ -211,9 +264,14 @@ impl PrebootPrimaryConfig for LiminePrimaryConfig {
 	const BAD_MEMORY_REPORTED: bool = true;
 }
 
+/// A simple Oro-compatible memory region type; mapped to from Limine
+/// memory region types by the [`make_memory_map_iterator`] function.
 struct LimineMemoryRegion {
+	/// The base address of the memory region.
 	base: u64,
+	/// The length of the memory region.
 	length: u64,
+	/// The Oro memory region type.
 	entry_type: MemoryRegionType,
 }
 
