d8dbd01df3287f0192e7e2133230a1fd3052b079
Josh Junon <junon@oro.sh>
2024-04-28 23:06:40 +0200

    common: add proc-macro crate [no ci]

---
+++ oro-common-proc/src/lib.rs
@@ -0,0 +1,7 @@
+//! Common proc macros used by the `oro-common` crate.
+//!
+//! > **NOTE:** Do NOT use this crate directly. It is only intended
+//! > to be used by the `oro-common` crate; anything meant to be used
+//! > by other crates will be re-exported by `oro-common`.
+#![deny(missing_docs, clippy::missing_docs_in_private_items)]
+#![allow(clippy::module_name_repetitions, clippy::missing_errors_doc)]
