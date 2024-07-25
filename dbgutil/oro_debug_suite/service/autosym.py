2134212fa0bd6c5737e5909610118f510096b530
Josh Junon <junon@oro.sh>
2024-07-25 17:17:25 +0200

    dbgutil: make autosym a litle more backwards compatible

--- dbgutil/oro_debug_suite/service/autosym.py
+++ dbgutil/oro_debug_suite/service/autosym.py
@@ -6,12 +6,19 @@ SYM_AARCH64_ATS1E1R = "oro_arch_aarch64::dbgutil::__oro_dbgutil_ATS1E1R"
 
 TRACKED_SYMBOLS = frozenset(set([("f", SYM_AARCH64_ATS1E1R)]))
 
-_DOMAINS = {"f": gdb.SYMBOL_FUNCTION_DOMAIN, "v": gdb.SYMBOL_VAR_DOMAIN}
+SYMBOL_FUNCTION_DOMAIN = (
+    gdb.SYMBOL_FUNCTION_DOMAIN
+    if hasattr(gdb, "SYMBOL_FUNCTION_DOMAIN")
+    else gdb.SYMBOL_FUNCTIONS_DOMAIN
+)
+
+_DOMAINS = {"f": SYMBOL_FUNCTION_DOMAIN, "v": gdb.SYMBOL_VAR_DOMAIN}
 
 
 class SymbolTracker(object):
     def __init__(self):
         self.__symbols = dict()
+        self.flush_all_on_new = False
 
     def get(self, sym):
         return self.__symbols.get(sym)
@@ -22,6 +29,9 @@ class SymbolTracker(object):
                 del self.__symbols[sym]
 
     def _on_objfile_loaded(self, objfile):
+        if self.flush_all_on_new:
+            self.__symbols.clear()
+
         for domain, sym in TRACKED_SYMBOLS:
             assert bool(
                 _DOMAINS[domain]
@@ -46,4 +56,10 @@ for objfile in gdb.objfiles():
 gdb.events.new_objfile.connect(
     lambda event: SYMBOLS._on_objfile_loaded(event.new_objfile)
 )
-gdb.events.free_objfile.connect(lambda event: SYMBOLS._on_objfile_freed(event.objfile))
+
+if hasattr(gdb.events, "free_objfile"):
+    gdb.events.free_objfile.connect(
+        lambda event: SYMBOLS._on_objfile_freed(event.objfile)
+    )
+else:
+    SYMBOLS.flush_all_on_new = True
