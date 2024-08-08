88171ea295b0c512786961bde00cf3ab506b95cd
Josh Junon <junon@oro.sh>
2024-08-08 02:45:43 +0200

    kernel: add module, ring, and port placeholder types / docs

---
+++ oro-kernel/src/module.rs
@@ -0,0 +1,60 @@
+//! Implements Oro module instances in the kernel.
+#![allow(clippy::module_name_repetitions)]
+#![allow(dead_code)] // TODO(qix-)
+
+use crate::id::{Id, IdType};
+
+/// A singular module instance.
+///
+/// Modules are effectively executables in the Oro ecosystem,
+/// loading similarly to processes in a traditional operating system.
+/// By themselves, modules do not do anything - it is when they are
+/// mounted onto a ring as an instance (hence "module instance")
+/// that they are effectively spawned and executed.
+///
+/// The kernel does not keep modules in memory; only module instances.
+///
+/// Further, not every module instance comes from a discrete module;
+/// on the root ring, the kernel mounts several built-in modules
+/// as instances to interact with system resources at a very low level.
+/// These are often referred to as "built-in modules" or "kernel modules".
+/// Unlike e.g. Linux, kernel modules are not extensible nor can they be
+/// added via user configuration; they are hard-coded into the kernel,
+/// and are often architecture-specific.
+///
+/// Typically the bootloader will have some means by which to load modules
+/// as instances onto the root ring, since without any additional application-
+/// specific modules, the kernel is effectively useless (will do nothing on
+/// boot). The preboot routine (that jumps to the kernel, see [`oro_common::boot_to_kernel()`])
+/// provides a means for memory-mapped portable executables (PEs) to be loaded
+/// onto the root ring as instances.
+///
+/// Those instances will have the highest privilege level, and will be able
+/// to interact with the kernel directly via the built-in modules, and
+/// from there can spawn additional rings and instances as needed to
+/// bootstrap the rest of the system as they see fit.
+///
+/// # Module IDs
+/// Module IDs internally are just the offset of the module instance
+/// in the arena pool, divided by the size of the arena slot. Put simply,
+/// if you think of the arena pool as an array of module instances,
+/// the module ID is the index of the module instance in that array.
+///
+/// Module instances, rings, and ports all have their own ID spaces.
+/// This means that a module instance, ring, and port can all have the
+/// same ID, and they will not conflict with each other.
+///
+/// In no case should a module's ID be used to indicate its functionality,
+/// version, or other metadata. It is purely an internal identifier, and
+/// should be treated as random. It is only valid for the lifetime of the
+/// module instance, and should not be stored or used outside of that context.
+/// They should be treated like process IDs in a traditional operating system.
+pub struct ModuleInstance {
+	/// The instance ID. This is unique for each module instance,
+	/// but can be re-used if instances are destroyed.
+	///
+	/// It is the offset of the arena slot into the arena pool.
+	pub id:        u32,
+	/// The module ID from which this instance was spawned.
+	pub module_id: Id<{ IdType::Module }>,
+}
---
+++ oro-kernel/src/ring.rs
@@ -0,0 +1,31 @@
+//! Implements Oro rings in the kernel.
+
+#![allow(dead_code)] // TODO(qix-)
+
+/// A singular ring.
+///
+/// Rings are collections of [`crate::module::ModuleInstance`]s.
+/// They also form the primary security boundary in the Oro ecosystem.
+///
+/// Module instances are mounted onto rings, allowing the instances to
+/// see all other instances on the ring, as well as child rings.
+///
+/// However, module instances on a ring cannot see 'sibling' or parent
+/// rings, or anything on them, under any circumstance. This is enforced
+/// by the kernel. The resources they have access to are limited to those
+/// explicitly granted to them by the parent ring via [`crate::port::Port`]s.
+///
+/// Rings have exactly one parent ring, and can have any number of child
+/// rings. The root ring is the only ring that has no parent ring, and is
+/// spawned by the kernel itself. Any boot module instances put onto
+/// the root ring are effectively at the highest privilege level of the
+/// system, and can interact with the kernel directly. Child rings may
+/// only do so if one of the root ring's module instances has granted
+/// them such access via a port.
+pub struct Ring {
+	/// The ring ID.
+	///
+	/// This is unique for each ring, but can be re-used if rings are destroyed.
+	/// It is the offset of the arena slot into the arena pool.
+	id: u32,
+}
