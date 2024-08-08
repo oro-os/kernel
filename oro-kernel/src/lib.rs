5b28d70f1b364070d08d7becfb85d094716e4ee6
Josh Junon <git@josh.junon.me>
2024-08-09 00:46:30 +0200

    kernel: initial shared registry arena allocation

--- oro-kernel/src/lib.rs
+++ oro-kernel/src/lib.rs
@@ -8,18 +8,29 @@
 	clippy::integer_division,
 	clippy::missing_docs_in_private_items
 )]
-// NOTE(qix-): This isn't strictly necessary but is on track for acceptance,
+#![allow(
+	clippy::module_name_repetitions,
+	clippy::struct_field_names,
+	clippy::too_many_lines
+)]
+// NOTE(qix-): `adt_const_params` isn't strictly necessary but is on track for acceptance,
 // NOTE(qix-): and the open questions (e.g. mangling) are not of concern here.
 // NOTE(qix-): https://github.com/rust-lang/rust/issues/95174
+// NOTE(qix-):
+// NOTE(qix-): `const_refs_to_static` is necessary for the `RegistryTarget` trait,
+// NOTE(qix-): which requires a static reference to the registry as a const trait
+// NOTE(qix-): item. This feature is on track for stabilization:
+// NOTE(qix-): https://github.com/rust-lang/rust/issues/128183
 #![allow(incomplete_features)]
-#![feature(adt_const_params)]
+#![feature(adt_const_params, const_refs_to_static)]
 
 pub(crate) mod id;
 pub(crate) mod module;
 pub(crate) mod port;
+pub(crate) mod registry;
 pub(crate) mod ring;
 
-use core::mem::MaybeUninit;
+use core::{mem::MaybeUninit, str::FromStr};
 use oro_arch::Target;
 use oro_common::{
 	arch::Arch,
@@ -32,12 +43,14 @@ use oro_common::{
 	sync::spinlock::unfair_critical::UnfairCriticalSpinlock,
 };
 
+/// The type of the physical address translator we'll use system-wide.
+type PhysicalTranslator = OffsetPhysicalAddressTranslator;
 /// The type of PFA we'll use system-wide.
 // NOTE(qix-): This is explicitly defined here since Rust doesn't have
 // NOTE(qix-): any way to define a "generic static" of sorts. TAIT does not
 // NOTE(qix-): work here. It's also why registries are defined in this file
 // NOTE(qix-): and not in their own type modules.
-type Pfa = FiloPageFrameAllocator<OffsetPhysicalAddressTranslator>;
+type Pfa = FiloPageFrameAllocator<PhysicalTranslator>;
 
 /// Holds the shared PFA.
 static mut PFA: MaybeUninit<UnfairCriticalSpinlock<Pfa>> = MaybeUninit::uninit();
@@ -92,6 +105,7 @@ pub enum CoreType {
 /// The `core_config` parameter must be properly initialized.
 /// Specifically, all safety requirements must be met, such as
 /// marking exactly one core as primary.
+#[allow(clippy::missing_panics_doc)] // XXX DEBUG
 pub unsafe fn boot(core_config: &CoreConfig) -> ! {
 	#[allow(clippy::missing_docs_in_private_items)]
 	macro_rules! wait_for_all_cores {
@@ -169,7 +183,77 @@ pub unsafe fn boot(core_config: &CoreConfig) -> ! {
 
 	if core_config.core_type == CoreType::Primary {
 		dbg!("kernel", "kernel transfer ok");
+
+		// Initialize the registries.
+		{
+			use crate::registry::RegistryTarget;
+
+			#[allow(clippy::missing_docs_in_private_items)]
+			macro_rules! create_registry {
+				($ty:ty, $segment:ident) => {
+					#[allow(clippy::missing_docs_in_private_items)]
+					const _: () = {
+						// SAFETY(qix-): While this is "mutable", it's only ever able to be mutated
+						// SAFETY(qix-): by the `initialize_registry()` function, which is called exactly
+						// SAFETY(qix-): once per registry.
+						static mut REGISTRY: core::mem::MaybeUninit<
+							crate::registry::Registry<$ty, PhysicalTranslator, Pfa>,
+						> = core::mem::MaybeUninit::uninit();
+
+						unsafe impl crate::registry::RegistryTarget for $ty {
+							type Alloc = Pfa;
+							type PhysicalAddressTranslator = PhysicalTranslator;
+
+							// SAFETY(qix-): This is not used by the registry until it is initialized.
+							const REGISTRY_PTR: *const crate::registry::Registry<
+								Self,
+								Self::PhysicalAddressTranslator,
+								Self::Alloc,
+							> = unsafe { REGISTRY.as_ptr() };
+
+							unsafe fn initialize_registry(
+								segment: <<Target as Arch>::AddressSpace as AddressSpace>::SupervisorSegment,
+								translator: Self::PhysicalAddressTranslator,
+							) {
+								REGISTRY.write(crate::registry::Registry::new(&*PFA.as_ptr(), translator, segment));
+							}
+						}
+					};
+
+					// SAFETY(qix-): Assuming the address space is implemented correctly
+					// SAFETY(qix-): by the architecture-specific crates, this is safe
+					// SAFETY(qix-): because cloning the address space handle prescribes
+					// SAFETY(qix-): that each new handle only touches a non-overlapping
+					// SAFETY(qix-): segment, and no others, exclusively. This is the case
+					// SAFETY(qix-): as all registry segments are prescribed by the architecture
+					// SAFETY(qix-): address space traits as non-overlapping.
+					<$ty>::initialize_registry(
+						<<Target as ::oro_common::arch::Arch>::AddressSpace as ::oro_common::mem::mapper::AddressSpace>::$segment(),
+						translator.clone(),
+					);
+				};
+			}
+
+			create_registry!(
+				crate::module::ModuleInstance,
+				kernel_module_instance_registry
+			);
+			create_registry!(crate::port::Port, kernel_port_registry);
+			create_registry!(crate::ring::Ring, kernel_ring_registry);
+		}
+
+		Target::strong_memory_barrier();
 	}
+
+	wait_for_all_cores!();
+
+	// XXX DEBUG
+	let module = module::ModuleInstance {
+		id:        0,
+		module_id: id::Id::from_str("M-1234ABCD5678EFGH9012IJKL3").unwrap(),
+	};
+	let _ref_module: self::registry::Ref<module::ModuleInstance> =
+		self::registry::Ref::from(module).expect("failed to allocate");
 
 	Target::halt()
 }
