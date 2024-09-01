1bfce9c74ef5f9cb45dc1a7e07291e570ac44895
Josh Junon <junon@oro.sh>
2024-09-01 14:49:09 +0200

    x86_64+aarch64+limine: use `asm_buffer!` macro for boot stubs and simplify linker

--- oro-boot/src/target/x86_64.rs
+++ oro-boot/src/target/x86_64.rs
@@ -1,7 +1,7 @@
 //! Contains the transfer stubs when the kernel is being switched to
 //! from the preboot environment on x86_64.
 
-use core::{arch::asm, ptr::from_ref};
+use core::arch::asm;
 use oro_arch_x86_64::mem::{
 	address_space::{AddressSpaceHandle, AddressSpaceLayout},
 	paging::PageTableEntry,
@@ -13,6 +13,7 @@ use oro_common::mem::{
 	pfa::alloc::{PageFrameAllocate, PageFrameFree},
 	translate::PhysicalAddressTranslator,
 };
+use oro_common_proc::asm_buffer;
 
 pub type SupervisorHandle = AddressSpaceHandle;
 pub type AddressSpace = AddressSpaceLayout;
@@ -29,12 +30,17 @@ const STUBS_SEGMENT_DESCRIPTOR: AddressSegment = AddressSegment {
 	intermediate_entry_template: PageTableEntry::new().with_present().with_writable(),
 };
 
-extern "C" {
-	/// The start of the transfer stubs.
-	pub static _ORO_STUBS_START: u64;
-	/// The end of the transfer stubs.
-	pub static _ORO_STUBS_LEN: u64;
-}
+/// The stub machine code to be executed in order to
+/// jump to the kernel.
+const STUBS: &[u8] = &asm_buffer! {
+	// Load the new page table base address.
+	"mov cr3, r9",
+	// Set the stack
+	"mov rsp, r10",
+	// Push a return value of 0 onto the stack to prevent accidental returns
+	"push 0",
+	"jmp r11",
+};
 
 /// Returns the target virtual address of the stubs based on
 /// the current CPU paging level.
@@ -56,8 +62,13 @@ pub unsafe fn prepare_transfer<
 	pat: &P,
 ) -> crate::Result<()> {
 	debug_assert!(
-		(from_ref(&_ORO_STUBS_LEN) as usize) <= 4096,
+		STUBS.len() <= 4096,
 		"transfer stubs are larger than a 4KiB page"
+	);
+	debug_assert_ne!(
+		STUBS.len(),
+		0,
+		"transfer stubs must have a length greater than 0",
 	);
 
 	// Map in the recursive entry.
@@ -66,61 +77,39 @@ pub unsafe fn prepare_transfer<
 	// Allocate and map in the transfer stubs
 	let stubs_base = target_address();
 
-	let stub_start = from_ref(&_ORO_STUBS_START) as usize;
-	let stub_len = from_ref(&_ORO_STUBS_LEN) as usize;
-
-	debug_assert!(
-		stub_start & 0xFFF == 0,
-		"transfer stubs must be 4KiB aligned: {stub_start:016X}",
-	);
-	debug_assert!(
-		stub_len & 0xFFF == 0,
-		"transfer stubs length must be a multiple of 4KiB: {stub_len:X}",
-	);
-	debug_assert!(
-		stub_len > 0,
-		"transfer stubs must have a length greater than 0: {stub_len:X}",
-	);
-
-	let num_pages = (stub_len + 4095) >> 12;
-
-	let source = stub_start as *const u8;
+	let source = &STUBS[0] as *const u8;
 	let dest = stubs_base as *mut u8;
 
 	let current_mapper = AddressSpaceLayout::current_supervisor_space(pat);
 
-	for page_offset in 0..num_pages {
-		let phys = alloc
-			.allocate()
-			.expect("failed to allocate page for transfer stubs (out of memory)");
+	let phys = alloc
+		.allocate()
+		.expect("failed to allocate page for transfer stubs (out of memory)");
 
-		let virt = stubs_base + page_offset * 4096;
+	// Map into the target kernel page tables
+	(&STUBS_SEGMENT_DESCRIPTOR)
+		.map(mapper, alloc, pat, stubs_base, phys)
+		.expect("failed to map page for transfer stubs for kernel address space");
 
-		// Map into the target kernel page tables
-		(&STUBS_SEGMENT_DESCRIPTOR)
-			.map(mapper, alloc, pat, virt, phys)
-			.expect("failed to map page for transfer stubs for kernel address space");
+	// Attempt to unmap it from the current address space.
+	// If it's not mapped, we can ignore the error.
+	(&STUBS_SEGMENT_DESCRIPTOR)
+		.unmap(&current_mapper, alloc, pat, stubs_base)
+		.or_else(|e| {
+			if e == UnmapError::NotMapped {
+				Ok(0)
+			} else {
+				Err(e)
+			}
+		})
+		.expect("failed to unmap page for transfer stubs from current address space");
 
-		// Attempt to unmap it from the current address space.
-		// If it's not mapped, we can ignore the error.
-		(&STUBS_SEGMENT_DESCRIPTOR)
-			.unmap(&current_mapper, alloc, pat, virt)
-			.or_else(|e| {
-				if e == UnmapError::NotMapped {
-					Ok(0)
-				} else {
-					Err(e)
-				}
-			})
-			.expect("failed to unmap page for transfer stubs from current address space");
+	// Now map it into the current mapper so we can access it.
+	(&STUBS_SEGMENT_DESCRIPTOR)
+		.map(&current_mapper, alloc, pat, stubs_base, phys)
+		.expect("failed to map page for transfer stubs in current address space");
 
-		// Now map it into the current mapper so we can access it.
-		(&STUBS_SEGMENT_DESCRIPTOR)
-			.map(&current_mapper, alloc, pat, virt, phys)
-			.expect("failed to map page for transfer stubs in current address space");
-	}
-
-	dest.copy_from(source, stub_len);
+	dest.copy_from(source, STUBS.len());
 
 	Ok(())
 }
@@ -149,34 +138,4 @@ pub unsafe fn transfer(
 		in("r12") stubs_addr,
 		options(noreturn)
 	);
-}
-
-/// Transfer stubs for the x86_64 architecture.
-///
-/// The following values need to be pushed onto the stack before
-/// jumping to the stubs. Push them *in this order*; do not reverse them
-///
-/// - The new page table base address (cr3).
-/// - The new stack pointer.
-/// - The new instruction pointer.
-///
-/// # Safety
-/// This function is meant to be called by the [`transfer()`]
-/// function and nowhere else.
-///
-/// The transfer stubs MUST be 4KiB page aligned AND be a multiple of 4KiB.
-#[naked]
-#[no_mangle]
-#[link_section = ".oro_xfer_stubs.entry"]
-unsafe extern "C" fn transfer_stubs() -> ! {
-	asm! {
-		// Load the new page table base address.
-		"mov cr3, r9",
-		// Set the stack
-		"mov rsp, r10",
-		// Push a return value of 0 onto the stack to prevent accidental returns
-		"push 0",
-		"jmp r11",
-		options(noreturn),
-	}
 }
