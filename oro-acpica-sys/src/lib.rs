7c230c48d225bdc03a754d10de0bce95858b6a3b
Josh Junon <git@josh.junon.me>
2024-09-06 06:53:04 +0200

    acpica-sys: move oro-os/oro-acpica-sys directly into repository

---
+++ oro-acpica-sys/src/lib.rs
@@ -0,0 +1,15 @@
+//! Low level, Oro-specific Rust bindings to the ACPICA library.
+//!
+//! > **NOTE:** This crate does not export any C functionality;
+//! > it is purely for data structures and constants.
+#![cfg_attr(not(test), no_std)]
+#![allow(
+	non_upper_case_globals,
+	non_camel_case_types,
+	non_snake_case,
+	rustdoc::bare_urls,
+	clippy::missing_safety_doc
+)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+include!(concat!(env!("OUT_DIR"), "/tablegen_macro.rs"));
