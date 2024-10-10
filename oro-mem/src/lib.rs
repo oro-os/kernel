6256a1bdbf1c61e126fc006f5fe8328d55d39e6a
Josh Junon <git@josh.junon.me>
2024-10-10 22:01:42 +0200

    mem: remove old 'kitchen sink' comment [no ci]

--- oro-mem/src/lib.rs
+++ oro-mem/src/lib.rs
@@ -4,15 +4,6 @@
 #![feature(core_intrinsics, never_type)]
 #![cfg_attr(debug_assertions, feature(naked_functions))]
 
-// TODO(qix-): This crate was originally a kitchen sink
-// TODO(qix-): "commons" crate for the entire kernel project.
-// TODO(qix-): It's since been split up into multiple crates,
-// TODO(qix-): leaving this one as the "memory" crate.
-//
-// TODO(qix-): There may still be some documentation and comments
-// TODO(qix-): that refer to the old "commons" crate, but they
-// TODO(qix-): should be updated as time goes on.
-
 pub mod mapper;
 pub mod pfa;
 pub mod region;
