e3bc57c0b5f17071668caa4686026dffca29305e
Josh Junon <junon@oro.sh>
2025-01-10 20:04:39 +0100

    sysabi: refactor to object registry ABI

--- oro-sysabi/src/arch/x86_64.rs
+++ oro-sysabi/src/arch/x86_64.rs
@@ -5,20 +5,26 @@ use core::{arch::asm, mem::transmute};
 
 use crate::syscall;
 
-#[expect(clippy::missing_docs_in_private_items)]
+/// Lowest level system call for x86_64.
 #[inline(always)]
-pub unsafe fn syscall_reg_get_raw(entity: u64, table: u64, key: u64) -> (u64, syscall::Error) {
-	let err: u32;
-	let ret: u64;
+pub unsafe fn syscall(
+	opcode: syscall::Opcode,
+	arg1: u64,
+	arg2: u64,
+	arg3: u64,
+	arg4: u64,
+) -> (syscall::Error, u64, u64) {
+	let mut err: u64 = opcode as u64;
+	let mut ret1: u64 = arg3;
+	let mut ret2: u64 = arg4;
 
 	asm!(
 		"syscall",
-		in("rax") syscall::Opcode::RegGet as u64,
-		in("rsi") table,
-		in("rdi") key,
-		in("r9") entity,
-		lateout("eax") err,
-		out("rdx") ret,
+		inlateout("rax") err,
+		in("rsi") arg1,
+		in("rdi") arg2,
+		inlateout("rdx") ret1,
+		inlateout("r9") ret2,
 		lateout("rcx") _,
 		lateout("r8") _,
 		lateout("r10") _,
@@ -41,43 +47,5 @@ pub unsafe fn syscall_reg_get_raw(entity: u64, table: u64, key: u64) -> (u64, sy
 		lateout("zmm15") _,
 	);
 
-	(ret, transmute::<u32, syscall::Error>(err))
-}
-
-#[expect(clippy::missing_docs_in_private_items)]
-#[inline(always)]
-pub unsafe fn syscall_reg_set_raw(entity: u64, table: u64, key: u64, value: u64) -> syscall::Error {
-	let err: u32;
-
-	asm!(
-		"syscall",
-		in("rax") syscall::Opcode::RegSet as u64,
-		in("rsi") table,
-		in("rdi") key,
-		in("r9") entity,
-		in("rdx") value,
-		lateout("eax") err,
-		lateout("rcx") _,
-		lateout("r8") _,
-		lateout("r10") _,
-		lateout("r11") _,
-		lateout("zmm0") _,
-		lateout("zmm1") _,
-		lateout("zmm2") _,
-		lateout("zmm3") _,
-		lateout("zmm4") _,
-		lateout("zmm5") _,
-		lateout("zmm6") _,
-		lateout("zmm7") _,
-		lateout("zmm8") _,
-		lateout("zmm9") _,
-		lateout("zmm10") _,
-		lateout("zmm11") _,
-		lateout("zmm12") _,
-		lateout("zmm13") _,
-		lateout("zmm14") _,
-		lateout("zmm15") _,
-	);
-
-	transmute::<u32, syscall::Error>(err)
+	(transmute::<u64, syscall::Error>(err), ret1, ret2)
 }
--- oro-sysabi/src/syscall.rs
+++ oro-sysabi/src/syscall.rs
@@ -17,9 +17,6 @@ pub type Result<T> = core::result::Result<T, Error>;
 /// As such, match arms should always have a catch-all arm (`_ => { ... }`) to handle otherwise
 /// unknown error codes.
 ///
-/// Further, the error code may pertain to a specific table. Those codes are not listed here.
-/// See _Bit Representation_ for more information.
-///
 /// # Error Permanence
 /// Error codes are marked as either **temporary** or **permanent** (sometimes determined by the
 /// table upon which is being operated).
@@ -31,21 +28,6 @@ pub type Result<T> = core::result::Result<T, Error>;
 /// In some select cases (marked as such), the error may be resolved in a newer version of the kernel,
 /// but application code should treat these errors as indicators of likely bugs or misconfigurations.
 ///
-/// # Bit Representation
-/// Unlike system call parameters, error codes are standardized
-/// as 32-bit unsigned integers. On 64-bit architectures, the upper
-/// 32-bits are reserved and should be assumed zero (the kernel will
-/// always return zero in the upper 32-bits).
-///
-/// Note that "reserved" here means that, in the future, the kernel
-/// reserves the right to begin using the upper 32-bits for additional
-/// information or error codes, at which time a means for retrieving those
-/// bits on 32-bit architectures will be provided.
-///
-/// Further, the high bit - bit 31 - is used to indicate whether the error
-/// belongs to the Oro registry (as in, an error occurred while issuing a
-/// syscall) or to the given table (as in, a table-specific error occurred).
-///
 /// # Error Precedence
 /// There is no stability or guarantee on the precedence of error codes. Applications should
 /// make no assumptions or inferrences about the parameters or context based on which error code
@@ -56,10 +38,8 @@ pub type Result<T> = core::result::Result<T, Error>;
 /// of the Oro registry before any other table-related error codes.
 ///
 /// It is, of course, guaranteed that [`Error::Ok`] will be returned for successful operations.
-///
-/// The error code system does not provide warnings or informational messages.
 #[non_exhaustive]
-#[repr(u32)]
+#[repr(u64)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Error {
 	// NOTE(qix-): Maintainers, please DO NOT replace or re-use error codes,
@@ -68,63 +48,40 @@ pub enum Error {
 	// NOTE(qix-):
 	// NOTE(qix-): Removed error codes should be marked as deprecated and kept
 	// NOTE(qix-): around until it's certain that no syscalls would return them.
-	/// The requested operation completed successfully.
+	/// The requested operation completed successfully. **Not an error.**
 	///
 	/// This error is **temporary**.
-	Ok             = 0,
+	Ok              = 0,
 	/// The operation code is invalid.
 	///
 	/// This error is **permanent**.
-	Invalid        = 1,
+	Invalid         = 1,
 	/// The requested operation code is valid, but some portion of the operation
 	/// is not implemented in the current version of the kernel (or environment).
 	///
 	/// This error is **permanent**.
-	NotImplemented = 2,
-	/// No such entity exists.
-	///
-	/// Note that this error code may be returned for valid entity identifiers
-	/// in cases where the requesting context does not have permission to access
-	/// the target entity or its context.
+	NotImplemented  = 2,
+	/// The requested key does not exist, or is outside the bounds of the object
+	/// (for list).
 	///
 	/// This error is **temporary**.
-	BadEntity      = 3,
-	/// The requested table does not exist.
-	///
-	/// This error is **permanent**.
-	BadTable       = 4,
-	/// The requested key does not exist, or is outside the bounds of the table
-	/// (for list-like tables).
-	///
-	/// The permanence of this error depends on the table.
-	///
-	/// - For fixed-key tables, this error is **permanent**.
-	/// - For list-like tables, this error is **temporary**.
-	/// - For dynamic tables, this error is **temporary**.
-	BadKey         = 5,
+	BadKey          = 3,
 	/// The given key is read-only and cannot be modified.
 	///
-	/// This may apply only to certain keys in a table, or to the entire table.
-	/// The error code makes no distinction between these cases.
+	/// This error is **permanent**.
+	ReadOnly        = 4,
+	/// The object is not a list; the requested opcode cannot be performed on it.
 	///
 	/// This error is **permanent**.
-	ReadOnly       = 6,
-	/// The given key is write-only and cannot be read.
-	///
-	/// This may apply only to certain keys in a table, or to the entire table.
-	/// The error code makes no distinction between these cases.
+	NotList         = 5,
+	/// The requested type is mismatched for the given key.
 	///
 	/// This error is **permanent**.
-	WriteOnly      = 7,
-	/// The table is not a list-like or dynamic table, and cannot have its length
-	/// queried.
+	WrongType       = 6,
+	/// The requested version mismatched.
 	///
 	/// This error is **permanent**.
-	NoLength       = 8,
-	/// The given table is a fixed-key table and cannot have its elements removed.
-	///
-	/// This error is **permanent**.
-	NoDelete       = 9,
+	VersionMismatch = 7,
 }
 
 /// Each individual operation that can be performed by the Oro registry.
@@ -132,34 +89,41 @@ pub enum Error {
 #[repr(u64)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Opcode {
-	/// Get a registry value by key.
-	RegGet = 0,
-	/// Set a registry value by key.
-	RegSet = 1,
+	/// Opens an object for reading and writing.
+	Open  = 0x8888_0000_0000_0000,
+	/// Closes an object handle opened with [`Opcode::Open`].
+	Close = 0x8888_0000_0000_0001,
+	/// Gets a value.
+	Get   = 0x8888_0000_0000_0002,
+	/// Sets a value.
+	Set   = 0x8888_0000_0000_0003,
 }
 
 /// Performs a branchless system call to get a registry value by key.
 ///
 /// Use [`reg_get`] for a safer alternative.
 ///
+/// Returns a tuple of the error code, the value, and the version.
+/// The value and version are only valid if the error code is [`Error::Ok`].
+///
 /// # Safety
 /// Any interpretation of the result value (first tuple value) is undefined
 /// if the error is not [`Error::Ok`].
 #[inline(always)]
 #[must_use]
-pub unsafe fn reg_get_raw(entity: u64, table: u64, key: u64) -> (u64, Error) {
+pub unsafe fn reg_get_raw(object_id: u64, key: u64, version: u64) -> (Error, u64, u64) {
 	#[cfg(target_arch = "x86_64")]
 	{
-		crate::arch::x86_64::syscall_reg_get_raw(entity, table, key)
+		crate::arch::x86_64::syscall(Opcode::Get, object_id, key, 0, version)
 	}
 
 	#[cfg(not(target_arch = "x86_64"))]
 	{
 		// NOTE(qix-): Avoids unused variable warnings.
-		let _ = entity;
-		let _ = table;
+		let _ = object_id;
 		let _ = key;
-		(0, Error::NotImplemented)
+		let _ = version;
+		(Error::NotImplemented, 0, 0)
 	}
 }
 
@@ -167,37 +131,156 @@ pub unsafe fn reg_get_raw(entity: u64, table: u64, key: u64) -> (u64, Error) {
 ///
 /// Use [`reg_set`] for a safer alternative.
 ///
+/// Returns a tuple of the error code and the new version.
+/// The new version is only valid if the error code is [`Error::Ok`].
+///
 /// # Safety
 /// The error code returned may be [`Error::Ok`] and should be checked.
 #[inline(always)]
 #[must_use]
-pub unsafe fn reg_set_raw(entity: u64, table: u64, key: u64, value: u64) -> Error {
+pub unsafe fn reg_set_raw(object_id: u64, key: u64, value: u64, version: u64) -> (Error, u64) {
 	#[cfg(target_arch = "x86_64")]
 	{
-		crate::arch::x86_64::syscall_reg_set_raw(entity, table, key, value)
+		let (err, _, version) =
+			crate::arch::x86_64::syscall(Opcode::Set, object_id, key, value, version);
+		(err, version)
 	}
 
 	#[cfg(not(target_arch = "x86_64"))]
 	{
 		// NOTE(qix-): Avoids unused variable warnings.
-		let _ = entity;
-		let _ = table;
+		let _ = object_id;
 		let _ = key;
+		let _ = version;
 		let _ = value;
+		(Error::NotImplemented, 0)
+	}
+}
+
+/// Performs a branchless system call to open an object.
+///
+/// Use [`reg_open`] for a safer alternative.
+///
+/// Returns the object's handle.
+///
+/// # Safety
+/// The returned handle is only valid if the error code is [`Error::Ok`].
+#[inline(always)]
+#[must_use]
+pub unsafe fn reg_open_raw(object_id: u64, key: u64) -> (Error, u64) {
+	#[cfg(target_arch = "x86_64")]
+	{
+		let (err, handle, _) = crate::arch::x86_64::syscall(Opcode::Open, object_id, key, 0, 0);
+		(err, handle)
+	}
+
+	#[cfg(not(target_arch = "x86_64"))]
+	{
+		// NOTE(qix-): Avoids unused variable warnings.
+		let _ = object_id;
+		let _ = key;
+		(Error::NotImplemented, 0)
+	}
+}
+
+/// Performs a branchless system call to close an object.
+///
+/// Use [`reg_close`] for a safer alternative.
+///
+/// # Safety
+/// The error code returned may be [`Error::Ok`] and should be checked.
+#[inline(always)]
+#[must_use]
+pub unsafe fn reg_close_raw(handle: u64) -> Error {
+	#[cfg(target_arch = "x86_64")]
+	{
+		let (err, _, _) = crate::arch::x86_64::syscall(Opcode::Close, handle, 0, 0, 0);
+		err
+	}
+
+	#[cfg(not(target_arch = "x86_64"))]
+	{
+		// NOTE(qix-): Avoids unused variable warnings.
+		let _ = handle;
 		Error::NotImplemented
 	}
 }
 
 /// Gets a registry value by key.
+///
+/// Returns a tuple of the value and the version.
 #[inline(always)]
-pub fn reg_get(entity: u64, table: u64, key: u64) -> Result<u64> {
-	let (ret, err) = unsafe { reg_get_raw(entity, table, key) };
-	if err == Error::Ok { Ok(ret) } else { Err(err) }
+pub fn reg_get(object_id: u64, key: u64, version: u64) -> Result<(u64, u64)> {
+	let (err, value, version) = unsafe { reg_get_raw(object_id, key, version) };
+	if err == Error::Ok {
+		Ok((value, version))
+	} else {
+		Err(err)
+	}
 }
 
 /// Sets a registry value by key.
+///
+/// Returns the new version of the value.
 #[inline(always)]
-pub fn reg_set(entity: u64, table: u64, key: u64, value: u64) -> Result<()> {
-	let err = unsafe { reg_set_raw(entity, table, key, value) };
+pub fn reg_set(object_id: u64, key: u64, value: u64, version: u64) -> Result<u64> {
+	let (err, version) = unsafe { reg_set_raw(object_id, key, value, version) };
+	if err == Error::Ok {
+		Ok(version)
+	} else {
+		Err(err)
+	}
+}
+
+/// Opens an object for reading and writing.
+///
+/// Returns the object's handle.
+///
+/// Must be closed with [`reg_close`] when no longer needed.
+#[inline(always)]
+pub fn reg_open(object_id: u64, key: u64) -> Result<u64> {
+	let (err, handle) = unsafe { reg_open_raw(object_id, key) };
+	if err == Error::Ok {
+		Ok(handle)
+	} else {
+		Err(err)
+	}
+}
+
+/// Closes an object handle.
+#[inline(always)]
+pub fn reg_close(handle: u64) -> Result<()> {
+	let err = unsafe { reg_close_raw(handle) };
 	if err == Error::Ok { Ok(()) } else { Err(err) }
+}
+
+/// Converts a literal string into a 64-bit object key.
+///
+/// # Panics
+/// Panics if the string is not 8 bytes or less.
+#[macro_export]
+macro_rules! key {
+	($key:literal) => {
+		const {
+			const KEY_RAW: &str = $key;
+
+			assert!(
+				KEY_RAW.len() <= 8,
+				concat!("object keys too long (must be <= 8 bytes): ", $key)
+			);
+
+			const KEY: &str = concat!($key, "\0\0\0\0\0\0\0\0");
+
+			let bytes = KEY.as_bytes();
+
+			((bytes[0] as u64) << 56)
+				| ((bytes[1] as u64) << 48)
+				| ((bytes[2] as u64) << 40)
+				| ((bytes[3] as u64) << 32)
+				| ((bytes[4] as u64) << 24)
+				| ((bytes[5] as u64) << 16)
+				| ((bytes[6] as u64) << 8)
+				| (bytes[7] as u64)
+		}
+	};
 }
