cb63467625a665b08036ba87787f603b58628bf9
Josh Junon <junon@oro.sh>
2025-01-15 20:55:48 +0100

    kernel: start monotonic counter at 0x0000_0001_0000_0000

--- oro-kernel/src/id.rs
+++ oro-kernel/src/id.rs
@@ -6,14 +6,16 @@ use core::sync::atomic::{AtomicU64, Ordering};
 ///
 /// Used for a variety of resources; all resources in the system are guaranteed
 /// to have a unique ID, even across resource types.
-static COUNTER: AtomicU64 = AtomicU64::new(1);
+///
+/// Always starts at `0x0000_0001_0000_0000`.
+static COUNTER: AtomicU64 = AtomicU64::new(0x0000_0001_0000_0000);
 
 /// Allocates a new globally unique identifier.
 ///
 /// Guaranteed to be unique across all cores in the system,
 /// and monotonically increasing. This function is lock-free.
 ///
-/// Guaranteed never to return 0.
+/// Always starts at `0x0000_0001_0000_0000`. Guaranteed never to return 0.
 #[inline]
 pub fn allocate() -> u64 {
 	COUNTER.fetch_add(1, Ordering::Relaxed)
