060dca9c00b36d8759eb850956cbb67d2a11312c
Josh Junon <junon@oro.sh>
2025-01-22 19:26:41 +0100

    oro: add debug output v0 `print[ln]!` macros

--- oro/src/runtime.rs
+++ oro/src/runtime.rs
@@ -115,20 +115,145 @@ pub unsafe fn force_crash() -> ! {
 pub mod id {
 	/// Kernel IDs; re-exported from [`oro_sysabi::id`].
 	pub use ::oro_sysabi::id as kernel;
-
-	/// Common non-kernel IDs.
-	///
-	/// > **NOTE:** This is not a comprehensive list of all IDs in the Oro ecosystem,
-	/// > but - notably - the IDs used by the standard library (`oro-std` or, in the future,
-	/// > `std`). They can be used directly or by using non-`core` types provided by the `[oro-]std` module.
-	pub mod common {
-		// TODO(qix-): Intentionally empty for now.
-	}
 }
 
-/// Syscall helper macros.
+/// Syscall low-level ABI elements.
 ///
-/// These are just re-exports from [`oro_sysabi::macros`].
+/// These are mostly just re-exports from [`oro_sysabi::macros`], some with
+/// more ergonomic names.
 pub mod syscall {
-	pub use ::oro_sysabi::{interface_slot, key, syscall_get as get, syscall_set as set, uses};
+	pub use ::oro_sysabi::{
+		interface_slot, key, syscall::Error, syscall_get as get, syscall_set as set, uses,
+	};
+}
+
+/// Common root ring interfaces.
+pub mod root_ring {
+	/// Debug output (version 0) interface abstraction.
+	pub mod debug_out_v0 {
+		use core::sync::atomic::{AtomicU64, Ordering::Relaxed};
+
+		use crate::{id, syscall};
+
+		/// The `KERNEL_DEBUG_OUT_V0` interface ID, or `0` if it's not
+		/// been resolved.
+		static DEBUG_OUT_V0_ID: AtomicU64 = AtomicU64::new(0);
+
+		/// Returns the `KERNEL_DEBUG_OUT_V0` interface ID, resolving
+		/// it if necessary.
+		///
+		/// Returns `None` if the interface could not be resolved.
+		pub fn id() -> Option<u64> {
+			let id = DEBUG_OUT_V0_ID.load(Relaxed);
+			if id == 0 {
+				let Ok(iface) = syscall::get!(
+					id::kernel::iface::KERNEL_IFACE_QUERY_BY_TYPE_V0,
+					id::kernel::iface::KERNEL_IFACE_QUERY_BY_TYPE_V0,
+					id::kernel::iface::ROOT_DEBUG_OUT_V0,
+					0
+				) else {
+					DEBUG_OUT_V0_ID.store(!0, Relaxed);
+					return None;
+				};
+
+				DEBUG_OUT_V0_ID.store(iface, Relaxed);
+				Some(iface)
+			} else if id == !0 {
+				// Positive failure cache; if there's no root ring
+				// debug interface, there won't be one in the future
+				// (at least, we assume).
+				None
+			} else {
+				Some(id)
+			}
+		}
+
+		/// Writes a byte slice to the debug output.
+		///
+		/// Note that this function behaves in a somewhat unusual way:
+		///
+		/// - There is no implicit encoding, though UTF-8 should be used
+		///   where possible.
+		/// - Newlines (`\n`, `0x0A`) are treated as line breaks, and flush
+		///   the buffer.
+		/// - The interface is **not thread safe**; multiple threads writing
+		///   to the debug output interface will result in interleaved output,
+		///   potentially flushing the buffer at unexpected times.
+		/// - The buffer has a per-line minimum and maximum before a line
+		///   is force-flushed.
+		/// - There is no `flush` command; simply send a newline to flush.
+		/// - Data is sent up in 8-byte increments over synchronous syscalls.
+		pub fn write_bytes(bytes: &[u8]) {
+			if bytes.is_empty() {
+				return;
+			}
+
+			let Some(iface) = id() else {
+				return;
+			};
+
+			for chunk in bytes.chunks(8) {
+				let mut word = 0u64;
+				for b in chunk {
+					word = (word << 8) | u64::from(*b);
+				}
+
+				syscall::set!(
+					id::kernel::iface::ROOT_DEBUG_OUT_V0,
+					iface,
+					0,
+					syscall::key!("write"),
+					word
+				)
+				.unwrap();
+			}
+		}
+
+		/// A [`core::fmt::Write`] implementation for the debug output interface.
+		///
+		/// This is a unit struct; it requires no initialization.
+		///
+		/// **Important:** Failures are _silent_; if no root ring debug output
+		/// exists, the data will be silently dropped.
+		pub struct DebugV0Write;
+
+		impl ::core::fmt::Write for DebugV0Write {
+			fn write_str(&mut self, s: &str) -> ::core::fmt::Result {
+				write_bytes(s.as_bytes());
+				Ok(())
+			}
+		}
+
+		/// `println!()` macro that prints to the root ring debug output (version 0)
+		/// interface.
+		///
+		/// **Important:** Failures are _silent_; if no root ring debug output
+		/// exists, the data will be silently dropped.
+		#[macro_export]
+		macro_rules! debug_v0_println {
+			($($arg:tt)*) => {
+				{
+					use $crate::root_ring::debug_out_v0::DebugV0Write;
+					use ::core::fmt::Write;
+					let _ = ::core::writeln!(&mut DebugV0Write, $($arg)*);
+				}
+			}
+		}
+
+		/// `print!()` macro that prints to the root ring debug output (version 0)
+		/// interface.
+		///
+		/// **Important:** Failures are _silent_; if no root ring debug output
+		/// exists, the data will be silently dropped.
+		#[macro_export]
+		macro_rules! debug_v0_print {
+			($($arg:tt)*) => {
+				{
+					use $crate::root_ring::debug_out_v0::DebugV0Write;
+					use ::core::fmt::Write;
+					let _ = ::core::write!(&mut DebugV0Write, $($arg)*);
+				}
+			}
+		}
+	}
 }
