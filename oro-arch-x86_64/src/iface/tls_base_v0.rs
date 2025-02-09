5c1ac08789442f9adf4532c759fd0bca5223f899
Josh Junon <junon@oro.sh>
2025-02-09 18:22:35 +0100

    x86_64: refactor TLS interface to allow cross-thread modification

--- oro-arch-x86_64/src/iface/tls_base_v0.rs
+++ oro-arch-x86_64/src/iface/tls_base_v0.rs
@@ -13,27 +13,17 @@ pub struct TlsBaseV0;
 
 impl KernelInterface<A> for TlsBaseV0 {
 	fn get(&self, thread: &Tab<Thread<A>>, index: u64, key: u64) -> InterfaceResponse {
+		let thread = oro_kernel::iface_resolve_thread_target!(crate::Arch, thread, index);
+
 		match key {
-			key!("name") => {
-				match index {
-					0 => InterfaceResponse::ok(key!("fs")),
-					1 => InterfaceResponse::ok(key!("gs")),
-					_ => InterfaceResponse::immediate(SysError::BadIndex, 0),
-				}
-			},
-			key!("base") => {
-				match index {
-					0 => {
-						debug_assert_eq!(crate::asm::get_fs_msr(), thread.with(|ctx| ctx.handle().fsbase));
-						InterfaceResponse::ok(crate::asm::get_fs_msr())
-					}
-					1 => {
-						debug_assert_eq!(crate::asm::get_gs_msr(), thread.with(|ctx| ctx.handle().gsbase));
-						InterfaceResponse::ok(crate::asm::get_gs_msr())
-					}
-					_ => InterfaceResponse::immediate(SysError::BadIndex, 0),
-				}
-			},
+			key!("fsbase") => {
+				let v = thread.with(|ctx| ctx.handle().fsbase);
+				InterfaceResponse::ok(v)
+			}
+			key!("gsbase") => {
+				let v = thread.with(|ctx| ctx.handle().gsbase);
+				InterfaceResponse::ok(v)
+			}
 			_ => InterfaceResponse::immediate(SysError::BadKey, 0),
 		}
 	}
@@ -45,29 +35,25 @@ impl KernelInterface<A> for TlsBaseV0 {
 		key: u64,
 		value: u64,
 	) -> InterfaceResponse {
-		match key {
-			key!("name") => InterfaceResponse::immediate(SysError::ReadOnly, 0),
-			key!("base") => {
-				match index {
-					0 => {
-						// Set the FS base pointer. Will get picked up by the
-						// next context switch.
-						thread.with_mut(|ctx| {
-							ctx.handle_mut().fsbase = value;
-						});
-					},
-					1 => {
-						// Set the GS base pointer. Will get picked up by the
-						// next context switch.
-						thread.with_mut(|ctx| {
-							ctx.handle_mut().gsbase = value;
-						});
-					},
-					_ => return InterfaceResponse::immediate(SysError::BadIndex, 0),
-				}
+		let thread = oro_kernel::iface_resolve_thread_target!(crate::Arch, thread, index);
 
+		match key {
+			key!("fsbase") => {
+				// Set the FS base pointer. Will get picked up by the
+				// next context switch.
+				thread.with_mut(|ctx| {
+					ctx.handle_mut().fsbase = value;
+				});
 				InterfaceResponse::ok(0)
-			}
+			},
+			key!("gsbase") => {
+				// Set the GS base pointer. Will get picked up by the
+				// next context switch.
+				thread.with_mut(|ctx| {
+					ctx.handle_mut().gsbase = value;
+				});
+				InterfaceResponse::ok(0)
+			},
 			_ => InterfaceResponse::immediate(SysError::BadKey, 0),
 		}
 	}
