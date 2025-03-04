1788017657899b1d18b2da8f1074c592909ddb3f
Josh Junon <junon@oro.sh>
2025-03-04 10:30:57 +0100

    x86_64: use safer and more robust boot stubs

--- oro-arch-x86_64/src/boot/secondary.rs
+++ oro-arch-x86_64/src/boot/secondary.rs
@@ -1,7 +1,8 @@
 //! Boot routines for secondary cores.
 
 use core::{
-	mem::MaybeUninit,
+	cell::UnsafeCell,
+	mem::offset_of,
 	sync::atomic::{
 		AtomicBool, AtomicU64,
 		Ordering::{Acquire, Relaxed, Release},
@@ -56,6 +57,45 @@ pub enum BootError {
 	SecondaryTimeout,
 }
 
+/// Secondary boot metadata structure.
+#[repr(C, align(4096))]
+struct BootMeta {
+	/// The boot stubs.
+	stubs: [u8; 0x800],
+	/// The GDTR.
+	gdtr: [u8; 6],
+	/// The LAPIC ID.
+	lapic_id: u8,
+	/// The primary flag.
+	primary_flag: AtomicU64,
+	/// The secondary flag.
+	secondary_flag: AtomicU64,
+	/// The actual stack pointer.
+	///
+	/// This is the stack pointer that the long mode stub will switch to,
+	/// and that serves as the final kernel stack pointer.
+	actual_stack_ptr: u64,
+	/// The actual CR3 value.
+	actual_cr3_ptr: u64,
+	/// The actual CR4 value.
+	actual_cr4_value: u64,
+	/// The actual CR0 value.
+	actual_cr0_value: u64,
+	/// The entry point.
+	entry_point: u64,
+	/// The null IDT.
+	null_idt: [u8; 6],
+	/// The CR4 bits.
+	cr4_bits: u32,
+	/// The GDT bytes.
+	gdt: [u8; 0x100],
+}
+
+const _: () = {
+	// Ensure the boot meta structure is the correct size.
+	assert::fits::<BootMeta, 4096>();
+};
+
 /// Boots a secondary core with the given local APIC and LAPIC ID.
 ///
 /// # Safety
@@ -69,39 +109,13 @@ pub unsafe fn boot_secondary(
 	secondary_lapic_id: u8,
 	stack_pages: usize,
 ) -> Result<(), BootError> {
-	// Some of these values aren't exact, but we want to align things nicely.
-	const LAPIC_ID_SIZE: u64 = 8; // Really 1
-	const PRIMARY_FLAG_SIZE: u64 = 8;
-	const SECONDARY_FLAG_SIZE: u64 = 8;
-	const LINEAR_OFFSET_SIZE: u64 = 8;
-	const ACTUAL_STACK_PTR_SIZE: u64 = 8;
-	const ACTUAL_CR3_PTR_SIZE: u64 = 8;
-	const ACTUAL_CR4_VALUE_SIZE: u64 = 8;
-	const ACTUAL_CR0_VALUE_SIZE: u64 = 8;
-	const ENTRY_POINT_SIZE: u64 = 8;
-	const NULLIDT_SIZE: u64 = 8; // Really 6
-	const CR4BITS_SIZE: u64 = 8; // Really 4
-	const GDTR_SIZE: u64 = 8; // Really 6
-	const TOP_RESERVE: u64 = LAPIC_ID_SIZE
-		+ SECONDARY_FLAG_SIZE
-		+ PRIMARY_FLAG_SIZE
-		+ LINEAR_OFFSET_SIZE
-		+ ACTUAL_STACK_PTR_SIZE
-		+ ACTUAL_CR3_PTR_SIZE
-		+ ACTUAL_CR4_VALUE_SIZE
-		+ ACTUAL_CR0_VALUE_SIZE
-		+ ENTRY_POINT_SIZE
-		+ NULLIDT_SIZE
-		+ CR4BITS_SIZE
-		+ GDTR_SIZE;
+	let stubs = Phys::from_address_unchecked(0x8000)
+		.as_ref::<UnsafeCell<BootMeta>>()
+		.unwrap();
 
 	// Make sure the stubs fit in the first half of the page...
-	debug_assert!(SECONDARY_BOOT_STUB.len() <= 0x400);
-	debug_assert!(SECONDARY_BOOT_LONG_MODE_STUB.len() <= 0x400);
-
-	// ... and that the GDT fits in the second part (minus TOP_RESERVE bytes).
-	let gdt_slice = crate::gdt::GDT.as_bytes();
-	debug_assert!(gdt_slice.len() <= (0x800 - TOP_RESERVE) as usize);
+	assert!(SECONDARY_BOOT_STUB.len() <= 0x400);
+	assert!(SECONDARY_BOOT_LONG_MODE_STUB.len() <= 0x400);
 
 	// Create a new supervisor address space based on the current address space.
 	let mapper = AddressSpaceLayout::duplicate_supervisor_space_shallow(primary_handle)
@@ -115,7 +129,6 @@ pub unsafe fn boot_secondary(
 
 	// Create a stack and map it into the secondary core's address space.
 	let kernel_stack_segment = AddressSpaceLayout::kernel_stack();
-
 	let last_stack_page_virt = kernel_stack_segment.range().1 & !0xFFF;
 
 	// "Forget" the entire segment (meaning simply to unmap but not
@@ -136,7 +149,7 @@ pub unsafe fn boot_secondary(
 	}
 
 	let mut bottom_stack_page_virt = last_stack_page_virt;
-	for stack_page_idx in 0..stack_pages {
+	for _ in 0..stack_pages {
 		bottom_stack_page_virt -= 4096;
 
 		let stack_phys = GlobalPfa
@@ -150,14 +163,8 @@ pub unsafe fn boot_secondary(
 		// rely on ESP (a 32-bit register) until the long mode trampoline
 		// is hit.
 		kernel_stack_segment
-			.remap(&mapper, bottom_stack_page_virt, stack_phys)
+			.map(&mapper, bottom_stack_page_virt, stack_phys)
 			.map_err(BootError::MapError)?;
-
-		if stack_page_idx == 0 {
-			AddressSpaceLayout::secondary_boot_stub_stack()
-				.remap(&mapper, 0x20000, stack_phys)
-				.map_err(BootError::MapError)?;
-		}
 	}
 
 	// Make sure that the bottom guard page is unmapped
@@ -169,142 +176,74 @@ pub unsafe fn boot_secondary(
 		Err(e) => return Err(BootError::UnmapError(e)),
 	}
 
-	// The 32-bit stack pointer is at 0x20000 + 4096 = 0x21000.
-	// This variable holds the long mode stack pointer that needs
-	// to be switched to when the long mode stubs start.
-	let stack_ptr = last_stack_page_virt;
-
-	// Copy the mapper into a well-known page (0x9000).
-	AddressSpaceLayout::copy_shallow_into(&mapper, 0x9000);
+	// Now copy the entire top-level page table into 0x9000.
+	let l4_table = mapper.base_phys().as_ptr_unchecked::<u8>();
+	let secondary_l4_table = Phys::from_address_unchecked(0x9000).as_mut_ptr_unchecked::<u8>();
+	secondary_l4_table.copy_from(l4_table, 4096);
 
 	// Write the stubs into the first half of the page.
 	// They live at 0x8000 (CS:IP = 0x0800:0x0000) and
 	// 0x8000 + 0x400 (CS:IP = 0x0800:0x0400) for the
 	// 16-bit and 64-bit stubs, respectively.
-	let stub_slice = core::slice::from_raw_parts_mut(
-		Phys::from_address_unchecked(0x8000).as_mut_ptr_unchecked::<u8>(),
-		SECONDARY_BOOT_STUB.len(),
-	);
+	let stub_slice = &mut (*stubs.get()).stubs[0..SECONDARY_BOOT_STUB.len()];
 	stub_slice.copy_from_slice(&SECONDARY_BOOT_STUB);
 
-	let long_mode_stub_slice = core::slice::from_raw_parts_mut(
-		Phys::from_address_unchecked(0x8000 + 0x400).as_mut_ptr_unchecked::<u8>(),
-		SECONDARY_BOOT_LONG_MODE_STUB.len(),
-	);
+	let long_mode_stub_slice =
+		&mut (*stubs.get()).stubs[0x400..0x400 + SECONDARY_BOOT_LONG_MODE_STUB.len()];
 	long_mode_stub_slice.copy_from_slice(&SECONDARY_BOOT_LONG_MODE_STUB);
 
-	// Write the GDT to the second half of the page.
-	// It lives at 0x8000 + 0x800 (CS:IP = 0x0800:0x0800).
-	let secondary_gdt = core::slice::from_raw_parts_mut(
-		Phys::from_address_unchecked(0x8000 + 0x800).as_mut_ptr_unchecked::<u8>(),
-		gdt_slice.len(),
+	// The GDT is copied as-is, just placed in a well-known location.
+	let gdt_slice = crate::gdt::GDT.as_bytes();
+	// TODO(qix-): Somehow make this a compile-time assertion.
+	debug_assert!(
+		gdt_slice.len() <= (*stubs.get()).gdt.len(),
+		"GDT is too large for the secondary boot stub page; increase the size of the stub page"
 	);
-	secondary_gdt.copy_from_slice(gdt_slice);
-
-	let mut meta_ptr = 0x9000 - TOP_RESERVE;
+	(*stubs.get()).gdt[..gdt_slice.len()].copy_from_slice(gdt_slice);
 
 	// Write the LAPIC ID.
-	debug_assert_eq!(meta_ptr, 0x8FA0);
-	Phys::from_address_unchecked(meta_ptr)
-		.as_mut_ptr_unchecked::<u8>()
-		.write_volatile(secondary_lapic_id);
-	meta_ptr += LAPIC_ID_SIZE;
-
-	// Write the linear offset.
-	debug_assert_eq!(meta_ptr, 0x8FA8);
-	// TODO(qix-): No longer used.
-	meta_ptr += LINEAR_OFFSET_SIZE;
+	(*stubs.get()).lapic_id = secondary_lapic_id;
 
 	// Zero the primary flag.
-	debug_assert_eq!(meta_ptr, 0x8FB0);
-	let primary_flag = {
-		assert::size_of::<AtomicU64, 8>();
-		let flag_ptr =
-			Phys::from_address_unchecked(meta_ptr).as_mut_ptr_unchecked::<MaybeUninit<AtomicU64>>();
-		(*flag_ptr).write(AtomicU64::new(0));
-		&*flag_ptr.cast::<AtomicU64>().cast_const()
-	};
-	meta_ptr += PRIMARY_FLAG_SIZE;
+	(*stubs.get()).primary_flag = AtomicU64::new(0);
 
 	// Zero the secondary flag.
-	debug_assert_eq!(meta_ptr, 0x8FB8);
-	let secondary_flag = {
-		assert::size_of::<AtomicU64, 8>();
-		let flag_ptr =
-			Phys::from_address_unchecked(meta_ptr).as_mut_ptr_unchecked::<MaybeUninit<AtomicU64>>();
-		(*flag_ptr).write(AtomicU64::new(0));
-		&*flag_ptr.cast::<AtomicU64>().cast_const()
-	};
-	meta_ptr += SECONDARY_FLAG_SIZE;
+	(*stubs.get()).secondary_flag = AtomicU64::new(0);
 
 	// Write the absolute entry point address of the Rust stub.
-	debug_assert_eq!(meta_ptr, 0x8FC0);
 	let entry_point_ptr = oro_kernel_x86_64_rust_secondary_core_entry as *const u8 as u64;
-	Phys::from_address_unchecked(meta_ptr)
-		.as_mut_ptr_unchecked::<u64>()
-		.write_volatile(entry_point_ptr);
-	meta_ptr += ENTRY_POINT_SIZE;
+	(*stubs.get()).entry_point = entry_point_ptr;
 
 	// Write the actual CR0 value so that the long mode stub can install it.
-	debug_assert_eq!(meta_ptr, 0x8FC8);
 	let cr0_value: u64 = crate::reg::Cr0::read().into();
-	Phys::from_address_unchecked(meta_ptr)
-		.as_mut_ptr_unchecked::<u64>()
-		.write_volatile(cr0_value);
-	meta_ptr += ACTUAL_CR0_VALUE_SIZE;
+	(*stubs.get()).actual_cr0_value = cr0_value;
 
 	// Write the actual CR4 value so that the long mode stub can install it.
-	debug_assert_eq!(meta_ptr, 0x8FD0);
 	let cr4_value: u64 = crate::reg::Cr4::load().into();
-	Phys::from_address_unchecked(meta_ptr)
-		.as_mut_ptr_unchecked::<u64>()
-		.write_volatile(cr4_value);
-	meta_ptr += ACTUAL_CR4_VALUE_SIZE;
+	(*stubs.get()).actual_cr4_value = cr4_value;
 
 	// Write the actual CR3 value so that the long mode stub can switch to it.
-	debug_assert_eq!(meta_ptr, 0x8FD8);
 	let cr3_phys = mapper.base_phys();
-	Phys::from_address_unchecked(meta_ptr)
-		.as_mut_ptr_unchecked::<u64>()
-		.write_volatile(cr3_phys.address_u64());
-	meta_ptr += ACTUAL_CR3_PTR_SIZE;
+	(*stubs.get()).actual_cr3_ptr = cr3_phys.address_u64();
 
 	// Write the real stack pointer so that the long mode stub can switch to it.
-	debug_assert_eq!(meta_ptr, 0x8FE0);
-	Phys::from_address_unchecked(meta_ptr)
-		.as_mut_ptr_unchecked::<u64>()
-		.write_volatile(stack_ptr as u64);
-	meta_ptr += ACTUAL_STACK_PTR_SIZE;
+	(*stubs.get()).actual_stack_ptr = last_stack_page_virt as u64;
 
-	// Zero the last 8 bytes of the page for the null IDT.
-	debug_assert_eq!(meta_ptr, 0x8FE8);
-	Phys::from_address_unchecked(meta_ptr)
-		.as_mut_ptr_unchecked::<u8>()
-		.write_bytes(0, NULLIDT_SIZE as usize);
-	meta_ptr += NULLIDT_SIZE;
+	// Zero out the null IDT.
+	(*stubs.get()).null_idt = [0; 6];
 
 	// Extract out the interesting bits of CR4 for the secondary core,
 	// without enabling anything that might screw up 16-bit initialization.
 	// We treat CR4 here as a 32-bit since this field is being accessed
 	// by the 32-bit stub.
-	debug_assert_eq!(meta_ptr, 0x8FF0);
 	let cr4_bits: u64 = crate::reg::Cr4::load().with_pge(false).into();
-	Phys::from_address_unchecked(meta_ptr)
-		.as_mut_ptr_unchecked::<u32>()
-		.write_volatile(cr4_bits as u32);
-	meta_ptr += CR4BITS_SIZE;
+	(*stubs.get()).cr4_bits = cr4_bits as u32;
 
 	// Write the GDT pointer into the last 6 bytes of the page.
-	debug_assert_eq!(meta_ptr, 0x8FF8);
-	let gdt_base: u32 = 0x8000 + 0x800;
-	let gdt_ptr = Phys::from_address_unchecked(meta_ptr).as_mut_ptr_unchecked::<u16>();
-	gdt_ptr.write(
-		u16::try_from(gdt_slice.len() - 1).expect("GDT is too large for the GDTR limit value"),
-	);
-	gdt_ptr.add(1).cast::<u32>().write_unaligned(gdt_base);
-	meta_ptr += GDTR_SIZE;
-
-	debug_assert_eq!(meta_ptr, 0x9000);
+	let gdt_base: u32 = 0x8000 + offset_of!(BootMeta, gdt) as u32;
+	let gdtr_descriptor_slice = &mut (*stubs.get()).gdtr;
+	gdtr_descriptor_slice[0..2].copy_from_slice(&(gdt_slice.len() as u16 - 1).to_le_bytes());
+	gdtr_descriptor_slice[2..6].copy_from_slice(&gdt_base.to_le_bytes());
 
 	// Finally, tell the processor to start executing at page 8 (0x8000).
 	// NOTE(qix-): Specifying other pages doesn't seem to work. The documentation
@@ -316,12 +255,12 @@ pub unsafe fn boot_secondary(
 	lapic.boot_core(secondary_lapic_id, 8);
 
 	// Tell the secondary core we're ready to go.
-	primary_flag.store(1, Release);
+	(*stubs.get()).primary_flag.store(1, Release);
 
 	// Wait for the secondary core to signal it's ready.
 	let mut ok = false;
 	for _ in 0..10_000_000 {
-		match secondary_flag.load(Acquire) {
+		match (*stubs.get()).secondary_flag.load(Acquire) {
 			1 => {
 				ok = true;
 				break;
@@ -330,7 +269,9 @@ pub unsafe fn boot_secondary(
 			err => {
 				// Tell the secondary we no longer want it to boot.
 				// Just as a precaution since it's already in an error state.
-				primary_flag.store(0xFFFF_FFFF_FFFF_FFFE, Release);
+				(*stubs.get())
+					.primary_flag
+					.store(0xFFFF_FFFF_FFFF_FFFE, Release);
 				return Err(BootError::SecondaryError(err));
 			}
 		}
@@ -338,7 +279,9 @@ pub unsafe fn boot_secondary(
 
 	if !ok {
 		// Tell the secondary we no longer want it to boot.
-		primary_flag.store(0xFFFF_FFFF_FFFF_FFFE, Release);
+		(*stubs.get())
+			.primary_flag
+			.store(0xFFFF_FFFF_FFFF_FFFE, Release);
 		return Err(BootError::SecondaryTimeout);
 	}
 
@@ -369,13 +312,13 @@ asm_buffer! {
 			//
 			// The zero IDT has been placed at 0x8FF0
 			// by the primary core.
-			"lidt [0x8FE8]",
+			"lidt [{LIDT_ADDR}]",
 
 			// Set the PAE and PGE bits in CR4,
 			// as well as any others that the primary
 			// core has set that we're interested in.
 			"mov eax, 0b10100000",
-			"mov ebx, [0x8FF0]",
+			"mov ebx, [{CR4_BITS}]",
 			"or eax, ebx",
 			"mov cr4, eax",
 
@@ -409,16 +352,15 @@ asm_buffer! {
 			"mov cr0, ebx",
 
 			// Load the GDT.
-			"lgdt [0x8FF8]",
-
-			// Set the stack pointer; the primary core
-			// has placed the stack at 0x20000, plus a
-			// page, so we can use the stack pointer
-			// at 0x21000.
-			"mov esp, 0x21000",
+			"lgdt [{GDTR}]",
 
 			// We can now jump to the long mode stub.
 			"ljmp 0x0008, 0x8400",
+		},
+		{
+			LIDT_ADDR = const 0x8000 + offset_of!(BootMeta, null_idt),
+			CR4_BITS = const 0x8000 + offset_of!(BootMeta, cr4_bits),
+			GDTR = const 0x8000 + offset_of!(BootMeta, gdtr),
 		},
 	};
 }
@@ -435,25 +377,32 @@ asm_buffer! {
 			".code64",
 
 			// Set the real stack pointer.
-			"mov rax, [0x8FE0]",
+			"mov rax, [{STACK_PTR}]",
 			"mov rsp, rax",
 
 			// Set the real CR3 value.
-			"mov rax, [0x8FD8]",
+			"mov rax, [{CR3}]",
 			"mov cr3, rax",
 
 			// Install the real CR0 value.
-			"mov rax, [0x8FC8]",
+			"mov rax, [{CR0}]",
 			"mov cr0, rax",
 
 			// Install the real CR4 value.
-			"mov rax, [0x8FD0]",
+			"mov rax, [{CR4}]",
 			"mov cr4, rax",
 
 			// Jump to the Rust kernel secondary core entry point.
 			"push 0",
-			"mov rax, [0x8FC0]",
+			"mov rax, [{ENTRY_POINT}]",
 			"jmp rax",
+		},
+		{
+			STACK_PTR = const 0x8000 + offset_of!(BootMeta, actual_stack_ptr),
+			CR3 = const 0x8000 + offset_of!(BootMeta, actual_cr3_ptr),
+			CR0 = const 0x8000 + offset_of!(BootMeta, actual_cr0_value),
+			CR4 = const 0x8000 + offset_of!(BootMeta, actual_cr4_value),
+			ENTRY_POINT = const 0x8000 + offset_of!(BootMeta, entry_point),
 		},
 	};
 }
@@ -467,9 +416,9 @@ unsafe extern "C" fn oro_kernel_x86_64_rust_secondary_core_entry() -> ! {
 	crate::interrupt::install_idt();
 	crate::asm::flush_tlb();
 
-	// Get references to the secondary boot flags.
-	let primary_flag = &*(0x8FB0 as *const AtomicU64);
-	let secondary_flag = &*(0x8FB8 as *const AtomicU64);
+	let stubs = Phys::from_address_unchecked(0x8000)
+		.as_ref::<UnsafeCell<BootMeta>>()
+		.unwrap();
 
 	// Pull the RSDP from the boot protocol
 	// SAFETY(qix-): We can just unwrap these values as they're guaranteed to be OK
@@ -484,7 +433,9 @@ unsafe extern "C" fn oro_kernel_x86_64_rust_secondary_core_entry() -> ! {
 	else {
 		// Tell the primary we failed.
 		dbg_err!("failed to get RSDT from ACPI tables");
-		secondary_flag.store(0xFFFF_FFFF_FFFF_FFFE, Release);
+		(*stubs.get())
+			.secondary_flag
+			.store(0xFFFF_FFFF_FFFF_FFFE, Release);
 		crate::asm::hang();
 	};
 
@@ -497,7 +448,9 @@ unsafe extern "C" fn oro_kernel_x86_64_rust_secondary_core_entry() -> ! {
 	else {
 		// Tell the primary we failed.
 		dbg_err!("failed to get LAPIC from ACPI tables");
-		secondary_flag.store(0xFFFF_FFFF_FFFF_FFFE, Release);
+		(*stubs.get())
+			.secondary_flag
+			.store(0xFFFF_FFFF_FFFF_FFFE, Release);
 		crate::asm::hang();
 	};
 
@@ -505,12 +458,14 @@ unsafe extern "C" fn oro_kernel_x86_64_rust_secondary_core_entry() -> ! {
 
 	// Make sure the LAPIC IDs match.
 	let lapic_id = lapic.id();
-	let given_lapic_id = (0x8FA0 as *const u8).read_volatile();
+	let given_lapic_id = (*stubs.get()).lapic_id;
 
 	if lapic_id != given_lapic_id {
 		// Tell the primary we failed.
 		dbg_err!("LAPIC ID mismatch: expected {given_lapic_id}, got {lapic_id}");
-		secondary_flag.store(0xFFFF_FFFF_FFFF_FFFE, Release);
+		(*stubs.get())
+			.secondary_flag
+			.store(0xFFFF_FFFF_FFFF_FFFE, Release);
 		crate::asm::hang();
 	}
 
@@ -519,12 +474,12 @@ unsafe extern "C" fn oro_kernel_x86_64_rust_secondary_core_entry() -> ! {
 	// Wait for the primary to tell us to continue.
 	let mut ok = false;
 	for _ in 0..100_000 {
-		match primary_flag.load(Acquire) {
+		match (*stubs.get()).primary_flag.load(Acquire) {
 			1 => {
 				// Tell the primary we're ready to go.
 				// SAFETY(qix-): Once we've written this value, the boot stub pages are no longer
 				// SAFETY(qix-): safe to write to.
-				(*secondary_flag).store(1, Release);
+				(*stubs.get()).secondary_flag.store(1, Release);
 				ok = true;
 				break;
 			}
@@ -540,6 +495,11 @@ unsafe extern "C" fn oro_kernel_x86_64_rust_secondary_core_entry() -> ! {
 			"secondary core timed out waiting for primary to signal to continue, or primary told \
 			 us to stop"
 		);
+
+		(*stubs.get())
+			.secondary_flag
+			.store(0xFFFF_FFFF_FFFF_FFFE, Release);
+
 		crate::asm::hang();
 	}
 
