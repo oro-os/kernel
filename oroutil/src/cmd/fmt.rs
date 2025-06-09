252d6edf02741128054f5b23f28d8320c26b1953
Josh Junon <junon@oro.sh>
2025-06-10 00:46:35 +0200

    oroutil: print unformatted filenames with `cargo oro fmt --check`

--- oroutil/src/cmd/fmt.rs
+++ oroutil/src/cmd/fmt.rs
@@ -6,20 +6,60 @@ pub fn run(args: FmtArgs) -> Result<(), Box<dyn std::error::Error>> {
 	// First, shell out to the normal rustfmt command via cargo.
 	let mut cmd = crate::util::cargo_command();
 
+	let mut success = true;
 	cmd.arg("fmt").arg("--all");
 
 	if args.check {
-		cmd.arg("--check");
-	}
+		let output = cmd
+			.arg("--")
+			.arg("--emit=json")
+			.stdout(std::process::Stdio::piped())
+			.spawn()?
+			.wait_with_output()?;
+		if !output.status.success() {
+			return Err(format!("`cargo fmt` failed with status: {}", output.status).into());
+		}
 
-	let status = cmd.spawn()?.wait()?;
+		let json_output = String::from_utf8(output.stdout)?;
 
-	if !status.success() {
-		return Err(format!("`cargo fmt` failed with status: {}", status).into());
+		if !json_output.is_empty() {
+			#[derive(serde::Deserialize)]
+			struct FmtOutput {
+				name:       String,
+				mismatches: Vec<RustMismatch>,
+			}
+
+			#[derive(serde::Deserialize)]
+			struct RustMismatch {
+				original: String,
+				expected: String,
+			}
+
+			let result = serde_json::from_str::<Vec<FmtOutput>>(&json_output)
+				.map_err(|e| format!("failed to parse `cargo fmt` output as JSON: {}", e))?;
+
+			success = success && result.is_empty();
+
+			for unformatted in result {
+				log::error!("file is not formatted correctly: {}", unformatted.name);
+				for mismatch in unformatted.mismatches {
+					let original = mismatch.original.trim_end_matches('\n');
+					let expected = mismatch.expected.trim_end_matches('\n');
+					if original != expected {
+						println!("\x1b[31m-{}\x1b[m", original);
+						println!("\x1b[32m+{}\x1b[m", expected);
+					}
+				}
+			}
+		}
+	} else {
+		let status = cmd.spawn()?.wait()?;
+		if !status.success() {
+			return Err(format!("`cargo fmt` failed with status: {}", status).into());
+		}
 	}
 
 	// Then find all toml files in the project and format them.
-	let mut success = true;
 	for toml_file in crate::util::glob_files(&["**/*.toml"])? {
 		let contents = std::fs::read_to_string(&toml_file)?;
 
@@ -27,6 +67,7 @@ pub fn run(args: FmtArgs) -> Result<(), Box<dyn std::error::Error>> {
 
 		if formatted != contents {
 			if args.check {
+				log::error!("file is not formatted correctly: {}", toml_file.display());
 				for (change, line) in
 					similar::utils::diff_lines(similar::Algorithm::Patience, &contents, &formatted)
 				{
@@ -50,6 +91,7 @@ pub fn run(args: FmtArgs) -> Result<(), Box<dyn std::error::Error>> {
 	}
 
 	if !success {
+		log::error!("some files are not formatted correctly, run `cargo oro fmt` to fix them");
 		std::process::exit(1);
 	}
 
